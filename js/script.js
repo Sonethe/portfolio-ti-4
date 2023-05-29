a6be601a2fef4257ed9bb9d2a37efc38be492427
Mazulev <sniperant@yandex.ru>
2023-05-29 21:52:03 +0300

    TI-6-2 fix

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+let page = document.querySelector('.page');
+let themeButton = document.querySelector('.theme-button');
+
+themeButton.onclick = function() {
+  page.classList.toggle('light-theme');
+  page.classList.toggle('dark-theme');
+};
